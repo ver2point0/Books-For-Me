7498e0e0b4df975a5d113df0765534cf4b006966
John McIntosh II <jmci10@hotmail.com>
2017-05-22 15:36:50 -0400

    add folder structure

--- app/src/main/java/com/ver2point0/android/booksforme/Activities/MainActivity.java
+++ app/src/main/java/com/ver2point0/android/booksforme/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ver2point0.android.booksforme;
+package com.ver2point0.android.booksforme.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ver2point0.android.booksforme.R;
 
 public class MainActivity extends AppCompatActivity {
 
